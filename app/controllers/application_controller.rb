3ea1a7a6989b1f4c3a2bccf0392a3d7b5e0cd283
jashema_panapa <jcpanapa@gmail.com>
2020-06-30 02:34:37 -0400

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get '/signup' do 
+
+  end 
+
+end
